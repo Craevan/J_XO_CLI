0e340f39be9441e84fe089d1e07445244e686ea8
a.siverskii <a.siverskii@gmail.com>
2022-03-26 11:50:30 +0300

    Board updated for flexible size

--- src/cli/xo/model/Board.java
+++ src/cli/xo/model/Board.java
@@ -1,38 +1,41 @@
 package cli.xo.model;
 
-import cli.xo.model.exceptions.AlreadyOccupiedException;
 import cli.xo.model.exceptions.InvalidPointException;
 
 public class Board {
 
-    private static final int FIELD_SIZE = 3;
     private static final int MIN_COORDINATE = 0;
-    private static final int MAX_COORDINATE = FIELD_SIZE;
 
-    private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
+    private final Figure[][] field;
+    private final int fieldSize;
+
+    public Board(final int fieldSize) {
+        this.fieldSize = fieldSize;
+        field = new Figure[fieldSize][fieldSize];
+    }
 
     public int getSize() {
-        return FIELD_SIZE;
+        return fieldSize;
     }
 
     public Figure getFigure(final Point point) throws InvalidPointException {
-        if (!checkPoint(point))
+        if (checkPoint(point))
             throw new InvalidPointException();
         return field[point.getX()][point.getY()];
     }
 
     public void setFigure(final Point point, final Figure figure) throws InvalidPointException {
-        if (!checkPoint(point))
+        if (checkPoint(point))
             throw new InvalidPointException();
         field[point.getX()][point.getY()] = figure;
     }
 
     private boolean checkPoint(final Point point) {
-        return checkCoordinate(point.getX()) && checkCoordinate(point.getY());
+        return checkCoordinate(point.getX(), field.length) || checkCoordinate(point.getY(), field[point.getX()].length);
     }
 
-    private boolean checkCoordinate(final int coordinate) {
-        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
+    private boolean checkCoordinate(final int coordinate, final int maxCoordinate) {
+        return coordinate < MIN_COORDINATE || coordinate >= maxCoordinate;
     }
 
 }
